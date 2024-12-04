b8c16be316acd852a871fbba0a90fc4e7ad82a8e
echeng-git <eccheng@ualberta.ca>
2024-12-04 11:27:19 -0700

    work in invkin

--- src/robot_arm.h
+++ src/robot_arm.h
@@ -11,29 +11,31 @@ struct Point3D {
     double z;
 };
 
-extern const float L1 = 1.2345;
-extern const float L2 = 1.2345;
-extern const float L3 = 1.2345;
+// TODO: Measure the linkage lengths.
+extern const double L1 = 1.2345;
+extern const double L2 = 1.2345;
+extern const double L3 = 1.2345;
 
-float deg2rad(float theta) {
+double deg2rad(double theta) {
     return theta * DEG_TO_RAD;
 }
 
-float rad2deg(float theta) {
+double rad2deg(double theta) {
     return theta * RAD_TO_DEG;
 }
 
 /// @brief Calculates the forward kinematics of the arm using the Denavit-Hartenberg convention.
-/// @param angles 
+/// It is assumed that the motors rotate above the Z, Y, and Z-axes, respectively.
+/// @param angles Motor angles.
 /// @return The point.
 BLA::Matrix<3> forwardKinematics(std::vector<double> angles) {
     std::for_each(angles.begin(), angles.end(), &deg2rad);
 
     double x = L1 * cos(angles[0]) + L2 * cos(angles[0]) * cos(angles[1]) +
-              L3 * cos(angles[0]) * cos(angles[1] + angles[2]);
+               L3 * cos(angles[0]) * cos(angles[1] + angles[2]);
 
     double y = L1 * sin(angles[0]) + L2 * sin(angles[0]) * cos(angles[1]) +
-              L3 * sin(angles[0]) * cos(angles[1] + angles[2]);
+               L3 * sin(angles[0]) * cos(angles[1] + angles[2]);
 
     double z = L2 * sin(angles[1]) + L3 * sin(angles[2] + angles[3]);
 
@@ -42,7 +44,37 @@ BLA::Matrix<3> forwardKinematics(std::vector<double> angles) {
 
 
 BLA::Matrix<3,3> estimateInitialJacobian() {
+    BLA::Matrix<3, 3> J = BLA::Eye<3,3>();
+    // BLA::Matrix<3,3> X = forwardKinematics();
+    // use forward kinematics to estimate the change.
+    double dX, dY, dZ;
+
+    // first column first motor
+
+    // second column second motor
+
+    // third column third motor
+}
+
+BLA::Matrix<3,3> recomputeJacobian(BLA::Matrix<3> angles) {
     
+    BLA::Matrix<3,3> J;
+
+    // TODO: Convert to radian
+
+    double theta1 = angles(0), theta2 = angles(1), theta3 = angles(2);
+
+    double c1 = cos(theta1), s1 = sin(theta1);
+    double c2 = cos(theta2), s2 = sin(theta2);
+    double c23 = cos(theta2 + theta3), s23 = sin(theta2 + theta3);
+
+    J = {
+        -s1 * (L1 + L2 * c2 + L3 * c23), -c1 * (L2 * s2 + L3 * s23), -L3 * c1 * s23,
+        c1 * (L1 + L2 * c2 + L3 * c23),  -s1 * (L2 * s2 + L3 * s23), -L3 * s1 * s23,
+        0,                               L2 * c2 + L3 * c23,         L3 * c23
+    };
+
+    return J;
 }
 
 #endif
