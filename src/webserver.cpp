aafd264ff9edc19225924b2ac4d69faee416c3b6
echeng-git <eccheng@ualberta.ca>
2024-12-06 22:45:35 -0700

    autorefresh, got grab state?

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -2,7 +2,7 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <cstdlib>
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 
 // NOTE: You will have to configure this. If you are on a public network,
 // then it would be better to create a hotspot to connect to.
@@ -16,9 +16,10 @@ int base = 0;
 int elbow = 0;
 int wrist = 0;
 int hand = 0;
+bool grab = false;
 
 // Initialize the LCD with the pins: RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(14, 12, 27, 26, 25, 33);
+// LiquidCrystal lcd(14, 12, 27, 26, 25, 33);
 
 /// @brief Handler for the root '/' endpoint, containing the main
 /// webpage. Also handles sending values to the robot.
@@ -32,56 +33,74 @@ void handleRoot() {
   if (server.hasArg("base")) base = server.arg("base").toInt();
   if (server.hasArg("elbow")) elbow = server.arg("elbow").toInt();
   if (server.hasArg("wrist")) wrist = server.arg("wrist").toInt();
+  if (server.hasArg("grab")) grab = server.arg("grab") == "1";
 
   // Display received values/
   sprintf(
     armsBuffer,
-    "\rBase: %03d | Elbow: %03d | Wrist: %03d", // Zero pad for simplicity in printing.
+    "\rBase: %03d | Elbow: %03d | Wrist: %03d | Grab: %s", // Zero pad for simplicity in printing.
     base,
     elbow,
-    wrist
+    wrist,
+    grab ? "ON " : "OFF"
   );
+
   // Serial monitor approach -- use LCD instead.
-  // Serial.flush();
-  // Serial.print('\r');
-  // Serial.print(armsBuffer);
+  Serial.flush();
+  Serial.print('\r');
+  Serial.print(armsBuffer);
 
-  lcd.clear();
-  lcd.print(armsBuffer);
+  // lcd.clear();
+  // lcd.print(armsBuffer);
 
-  // Use JavaScript AJAX to dynamically update the webpage with motor angles.
-  // TODO: It might be worth looking into moving this into a deficated HTML file and whatnot
-  message = "<html><head><title>CMPUT 312 3DOF ARM</title>"
-      "<script>"
-      "function refresh() {"
-      "  setTimeout(() => {"
-      "    fetch('/getAngles')"
-      "    .then(response => response.json())"
-      "    .then(data => {" 
-      "      document.getElementById('base').innerHTML = 'Base: ' + data.base;"
-      "      document.getElementById('elbow').innerHTML = 'Elbow: ' + data.elbow;"
-      "      document.getElementById('wrist').innerHTML = 'Wrist: ' + data.wrist;"
-      "    });"
-      "  }, 250);"
-      "}"
-      "window.onload = refresh;"
-      "</script>"
-      "</head>"
-      "<body><h1>Simple HTTP server</h1>"
-      "<div id='form-container'>"
-      "<form method='GET' action='/'>"
-      "<label>Base: </label><input type='number' name='base' min='0' max='360' placeholder='Base angle' value=''><br>"
-      "<label>Elbow: </label><input type='number' name='elbow' min='0' max='360' placeholder='Elbow angle' value=''><br>"
-      "<label>Wrist: </label><input type='number' name='wrist' min='0' max='360' placeholder='Wrist angle' value=''><br>"
-      "<input type='submit' value='Send'/><br>"
-      "</form>"
-      "</div>"
-      "<h2>Telemetry</h2><div id='angles'>"
-      "<p id='base'>Base: " + String(base) + "</p>"
-      "<p id='elbow'>Elbow: " + String(elbow) + "</p>"
-      "<p id='wrist'>Wrist: " + String(wrist) + "</p>"
-      "</div>"
-      "</body></html>";
+  // Dynamically update the webpage with motor angles.
+message = "<html><head><title>CMPUT 312 3DOF ARM</title>"
+    "<script>"
+    "let grabState = " + String(grab ? "true" : "false") + ";"
+    
+    "function toggleGrab() {"
+    "  grabState = !grabState;"
+    "  document.getElementById('grab-button').innerText = grabState ? 'Grab: ON' : 'Grab: OFF';"
+    "  document.getElementById('grab-input').value = grabState ? '1' : '0';"
+    "}"
+
+    "function updateTelemetry() {"
+    "  fetch('/getAngles')"
+    "    .then(response => response.json())"
+    "    .then(data => {"
+    "      document.getElementById('base').innerHTML = 'Base: ' + data.base;"
+    "      document.getElementById('elbow').innerHTML = 'Elbow: ' + data.elbow;"
+    "      document.getElementById('wrist').innerHTML = 'Wrist: ' + data.wrist;"
+    "      document.getElementById('grab').innerHTML = 'Grab: ' + (data.grab ? 'ON' : 'OFF');"
+    "    })"
+    "    .catch(err => console.error('Error fetching telemetry:', err));"
+    "}"
+
+    "setInterval(updateTelemetry, 500);"
+    
+    "window.onload = () => {"
+    "  updateTelemetry();"
+    "};"
+    "</script>"
+    "</head>"
+    "<body><h1>Simple HTTP server</h1>"
+    "<div id='form-container'>"
+    "<form method='GET' action='/'>"
+    "<label>Base: </label><input type='number' name='base' min='0' max='360' placeholder='Base angle' value='" + String(base) + "'><br>"
+    "<label>Elbow: </label><input type='number' name='elbow' min='0' max='360' placeholder='Elbow angle' value='" + String(elbow) + "'><br>"
+    "<label>Wrist: </label><input type='number' name='wrist' min='0' max='360' placeholder='Wrist angle' value='" + String(wrist) + "'><br>"
+    "<button type='button' id='grab-button' onclick='toggleGrab()'>Grab: " + (grab ? "ON" : "OFF") + "</button><br>"
+    "<input type='hidden' name='grab' id='grab-input' value='" + String(grab ? "1" : "0") + "'>"
+    "<input type='submit' value='Send'/><br>"
+    "</form>"
+    "</div>"
+    "<h2>Telemetry</h2><div id='angles'>"
+    "<p id='base'>Base: " + String(base) + "</p>"
+    "<p id='elbow'>Elbow: " + String(elbow) + "</p>"
+    "<p id='wrist'>Wrist: " + String(wrist) + "</p>"
+    "<p id='grab'>Grab: " + (grab ? "ON" : "OFF") + "</p>"
+    "</div>"
+    "</body></html>";
 
   // Send response with form and message
   server.send(200, "text/html", message);
@@ -91,7 +110,8 @@ void handleRoot() {
 void handleGetAngles() {
   String json = "{\"base\": " + String(base) + ", "
                 "\"elbow\": " + String(elbow) + ", "
-                "\"wrist\": " + String(wrist) + "}";
+                "\"wrist\": " + String(wrist) + ", "
+                 "\"grab\": " + String(grab ? "true" : "false") + "}";
   server.send(200, "application/json", json);
 }
 
