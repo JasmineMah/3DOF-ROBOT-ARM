4d4035ff77a11b67bd2b4446487fb97e23767aed
Eric <90707245+echeng-git@users.noreply.github.com>
2024-12-14 19:44:05 -0700

    Update secrets.h

--- src/headers/secrets.h
+++ src/headers/secrets.h
@@ -1,7 +1,7 @@
 #ifndef SECRETS
 #define SECRETS
 
-#define WIFI_SSID "WIFI-35C4" // Network name
-#define WIFI_PASSWORD "banana4268chord" // Password of network
+#define WIFI_SSID "" // Network name
+#define WIFI_PASSWORD "" // Password of network
 
 #endif
